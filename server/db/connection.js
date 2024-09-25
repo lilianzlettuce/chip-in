8fd690334dbc04cab53da8d562b89bbeeffc77d3
lettuce <57018651+lilianzlettuce@users.noreply.github.com>
2024-09-25 03:02:34 -0400

    deploying backend on render: modified scripts and config

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,6 +1,7 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
-const uri = process.env.ATLAS_URI || "";
+const uri = process.env.MONGODB_URI || "";
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
