43cc3dc22e3da528250436e85308934f3b915e5f
lettuce <57018651+lilianzlettuce@users.noreply.github.com>
2024-09-25 00:07:40 -0400

    connected mongodb to vercel, changed env vars labels to match

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,6 +1,8 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
-const uri = process.env.ATLAS_URI || "";
+//const uri = process.env.ATLAS_URI || "";
+const uri = process.env.MONGODB_URI || "";
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
