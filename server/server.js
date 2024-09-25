8344e3805bad0aac2a1e9f04d49f65ed870ec69f
lettuce <57018651+lilianzlettuce@users.noreply.github.com>
2024-09-25 04:23:25 -0400

    try to fix internal server error

--- server/server.js
+++ server/server.js
@@ -15,12 +15,12 @@ app.use("/record", records);
 //app.use("/api/record", records);
 
 //* Serve static assets in production, must be at this location of this file
-if (process.env.NODE_ENV === 'production') {
+/*if (process.env.NODE_ENV === 'production') {
   //*Set static folder up in production
   app.use(express.static('client/build'));
 
   app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
-}
+}*/
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
