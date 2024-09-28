a3daa808f4d47b7b26a0f1f57e0f686b9e45db1b
joylynpan <pan379@purdue.edu>
2024-09-28 16:49:20 -0400

    itegrate users with items

--- server/models/Item.js
+++ server/models/Item.js
@@ -3,8 +3,8 @@ import mongoose, { mongo }  from "mongoose";
 const itemSchema = new mongoose.Schema({
     name: { type: String, required: true},
     category: { type: String, required: true },
-    purchasedBy: { type: String, required: false },
-    sharedBetween: { type: [String], required: false},
+    purchasedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false },
+    sharedBetween: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false }],
     purchaseDate: { type: Date, required: false},
     expirationDate: { type: Date, required: false},
     cost: {type: Number, required: false}
