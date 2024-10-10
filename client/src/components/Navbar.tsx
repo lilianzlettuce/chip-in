994577f17bf3a412cb2bb9bc509a7d108f487e7b
lettuce <57018651+lilianzlettuce@users.noreply.github.com>
2024-10-09 21:10:18 -0400

    added sign out function

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import logo from "../assets/chip-in-logo1.png"
 
 import { useParams } from 'react-router-dom';
@@ -7,6 +7,13 @@ export default function Navbar() {
   const { householdId, userId } = useParams();
   console.log(`${householdId}, ${userId}`);
 
+  const navigate = useNavigate();
+
+  const signOut = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+  }
+
   return (
     <div>
       <nav className="fixed min-w-48 h-screen mr-4 border-r-4 border-black flex flex-col justify-between items-center">
@@ -14,12 +21,12 @@ export default function Navbar() {
           <img alt="ChipIn logo" className="h-20 inline" src={logo}></img>
         </NavLink>
 
-        <NavLink to="/create"
-            className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background 
+        <div onClick={signOut}
+            className="cursor-pointer inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background 
               transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 
               disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9 rounded-md px-3" >
-          Create Employee
-        </NavLink>
+          Sign Out
+        </div>
 
         <div className="flex flex-col justify-between items-center">
           <NavLink to={`/households/${householdId ? householdId : 1}/home`}
