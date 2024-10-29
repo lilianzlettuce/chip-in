414b262d3c65d706938c65ebd9b01c8a727ad58d
lettuce <57018651+lilianzlettuce@users.noreply.github.com>
2024-10-28 22:57:13 -0400

    modify user settings done

--- client/src/routes/Recipes.tsx
+++ client/src/routes/Recipes.tsx
@@ -1,6 +1,9 @@
+import Alerts from '../components/Alerts';
+
 export default function Recipes() {
     return (
         <div>
+            <Alerts />
             <h1>This is the recipes page, where AI makes your food for you.</h1>
         </div>
     );
