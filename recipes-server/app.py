91373472f981c2d606798710c3fb93494fd97049
Chelsea Lee <chellie109@gmail.com>
2024-11-15 13:59:20 -0500

    removed CORS stuff

--- recipes-server/app.py
+++ recipes-server/app.py
@@ -1,9 +1,7 @@
 from flask import Flask, request, jsonify
 from flaxrec import generation_function
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
 
 @app.route('/', methods=['GET'])
 def hello_world():
