ddd7360cd420e5e0dd21da8514279708ae189338
lettuce <57018651+lilianzlettuce@users.noreply.github.com>
2024-12-04 21:42:07 -0500

    removed employee records page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -47,7 +47,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <RecordList />
+        element: <Profile />
       },
       {
         path: "/profile",
